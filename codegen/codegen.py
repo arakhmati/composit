8438e46b302d5b428e1f48eca5154ed0b0b303a6
arakhmati <akhmed.rakhmati@gmail.com>
2023-03-04 21:43:25 -0500

    Added codegen library

---
+++ codegen/codegen.py
@@ -0,0 +1,385 @@
+import enum
+from dataclasses import dataclass, replace
+from typing import Union, Any, Optional
+
+
+class UpdateOperator(enum.Enum):
+    ADD = enum.auto()
+
+
+def get_indentation_prefix(indentation):
+    indentation_prefix = ""
+    if indentation:
+        indentation_prefix = " " * indentation
+    return indentation_prefix
+
+
+def concatenate_as_string(sequence, delimiter, indentation=0):
+    indentation_prefix = get_indentation_prefix(indentation)
+    return delimiter.join([f"{indentation_prefix}{element}" for element in sequence])
+
+
+@dataclass
+class Expression:
+    expression: Any
+
+    @property
+    def value(self):
+        return self.expression.value
+
+    def __repr__(self):
+        return f"{self.expression}"
+
+    def __lt__(self, other):
+        return Expression(BinaryOperation(self, other, "<"))
+
+    def __mul__(self, other):
+        return Expression(BinaryOperation(self, other, "*"))
+
+    def __add__(self, other):
+        return Expression(BinaryOperation(self, other, "+"))
+
+
+def literal(value):
+    @dataclass
+    class Literal:
+        value: Any
+
+        def __repr__(self):
+            return f"{self.value}"
+
+    return Expression(Literal(value))
+
+
+@dataclass
+class BinaryOperation:
+    left_side: Union["Variable", Expression]
+    right_side: Union["Variable", Expression]
+    operator: str
+
+    def __repr__(self):
+        left_side = self.left_side.value
+        right_side = self.right_side.value
+        return f"{left_side} {self.operator} {right_side}"
+
+    @property
+    def value(self):
+        return str(self)
+
+
+@dataclass
+class Identifier:
+    name: str
+
+    def __repr__(self):
+        return f"{self.name}"
+
+
+@dataclass
+class Type:
+    name: str
+    _pointer: bool = False
+    _const: bool = False
+    _constexpr: bool = False
+    _reference: bool = False
+    _restrict: bool = False
+    _alignment: Optional[int] = None
+
+    def __repr__(self):
+        name = self.name
+        if self._pointer:
+            name = f"{name}*"
+        if self._reference:
+            name = f"{name}&"
+        if self._restrict:
+            name = f"{name} __restrict__"
+        if self._alignment is not None:
+            name = f"{name} __attribute__((aligned({self._alignment})))"
+        if self._const:
+            name = f"const {name}"
+        if self._constexpr:
+            name = f"constexpr {name}"
+        return name
+
+    def pointer(self):
+        return replace(self, _pointer=True)
+
+    def const(self):
+        return replace(self, _const=True)
+
+    def reference(self):
+        return replace(self, _reference=True)
+
+    def restrict(self):
+        return replace(self, _restrict=True)
+
+    def constexpr(self):
+        return replace(self, _constexpr=True)
+
+    def aligned(self, value):
+        return replace(self, _alignment=value)
+
+
+@dataclass
+class Variable:
+    type: Type
+    identifier: Identifier
+
+    @property
+    def value(self):
+        return self.identifier
+
+    def __repr__(self):
+        return f"{self.type} {self.identifier}"
+
+    def __lshift__(self, value):
+        return Statement(Declare(self, value))
+
+    def __iadd__(self, value):
+        return AssignUpdate(self, value, UpdateOperator.ADD)
+
+    def __lt__(self, other):
+        return Expression(BinaryOperation(self, other, "<"))
+
+    def __getitem__(self, item):
+        return Subscript(self, item)
+
+    def __mul__(self, other):
+        return Expression(BinaryOperation(self, other, "*"))
+
+    def __add__(self, other):
+        return Expression(BinaryOperation(self, other, "+"))
+
+
+def variable(type, name) -> Variable:
+    return Variable(type, Identifier(name))
+
+
+AUTO = Type("auto")
+
+
+@dataclass
+class Subscript:
+    address: Variable
+    offset: Expression
+
+    def __repr__(self):
+        address = self.address.value
+        offset = self.offset.value
+        return f"{address}[{offset}]"
+
+    @property
+    def value(self):
+        return str(self)
+
+    def __iadd__(self, value):
+        return AssignUpdate(self, value, UpdateOperator.ADD)
+
+
+@dataclass
+class Statement:
+    statement: Any
+
+    def __repr__(self):
+        statement = self.statement
+        if isinstance(self.statement, Variable):
+            statement = str(self.statement.identifier)
+        return f"{statement};"
+
+
+@dataclass
+class Return:
+    statement: Statement
+
+    def __repr__(self):
+        return f"return {self.statement}"
+
+
+@dataclass
+class Assign:
+    left_side: Union[Variable, Subscript]
+    right_side: Expression
+    update_operator: Optional[UpdateOperator] = None
+
+    def __repr__(self):
+        return f"{self.left_side.value} = {self.right_side.value}"
+
+
+@dataclass
+class AssignUpdate:
+    left_side: Union[Variable, Subscript]
+    right_side: Expression
+    update_operator: Optional[UpdateOperator] = None
+
+    def __repr__(self):
+
+        if self.update_operator == UpdateOperator.ADD and self.right_side.value == 1:
+            return f"{self.left_side.value}++"
+
+        operator_to_string = {
+            UpdateOperator.ADD: "+=",
+        }
+        assign_operator = operator_to_string[self.update_operator]
+
+        return f"{self.left_side.value} {assign_operator} {self.right_side.value}"
+
+
+def add_in_place(variable, value):
+    variable += value
+    return variable
+
+
+@dataclass
+class Declare:
+    left_side: Variable
+    right_side: Expression
+
+    def __repr__(self):
+        return f"{self.left_side.type} {Assign(self.left_side, self.right_side)}"
+
+
+@dataclass
+class Block:
+    statements: list[Union[Statement, Return, "ForLoop"]]
+    indentation_level: Optional[int] = None
+    indentation: int = 4
+    
+    def set_indentation_level(self, indentation_level=0):
+        if self.indentation_level is not None:
+            return
+        self.indentation_level = indentation_level
+        for statement in self.statements:
+            if isinstance(statement, (ForLoop, Function)):
+                statement.set_indentation_level(indentation_level + 1)
+
+    def __repr__(self):
+        indentation = self.indentation_level * self.indentation if self.indentation_level is not None else 0
+        indentation_prefix = get_indentation_prefix(indentation)
+        delimiter = "\n"
+        return f"""\
+{indentation_prefix}{{
+{concatenate_as_string(self.statements, delimiter, indentation=indentation + self.indentation)}
+{indentation_prefix}}}"""
+
+
+@dataclass
+class ForLoop:
+    initialization_statement: Any
+    test_expression: Expression
+    update_statement: Any
+    body: Block
+
+    def set_indentation_level(self, indentation_level=0):
+        self.body.set_indentation_level(indentation_level)
+
+    def __repr__(self):
+        self.set_indentation_level()
+        return f"""\
+for ({self.initialization_statement}; {self.test_expression}; {self.update_statement})
+{self.body}"""
+
+
+@dataclass(kw_only=True)
+class Function:
+    return_type: Type
+    name: Identifier
+    arguments: list[Variable]
+    body: Block
+
+    _inline: bool = False
+    _static: bool = False
+
+    def set_indentation_level(self, indentation_level=0):
+        self.body.set_indentation_level(indentation_level)
+
+    def __repr__(self):
+        self.set_indentation_level()
+        delimiter = ", "
+        string = f"""\
+{self.return_type} {self.name}({concatenate_as_string(self.arguments, delimiter)})
+{self.body}"""
+
+        if self._inline:
+            string = f"inline {string}"
+
+        if self._static:
+            string = f"static {string}"
+
+        return string
+    def inline(self):
+        return replace(self, _inline=True)
+
+    def static(self):
+        return replace(self, _static=True)
+
+
+@dataclass
+class FunctionCall:
+    function_name: Identifier
+    arguments: list[Union[Identifier, Expression]]
+
+    def __repr__(self):
+        delimiter = ", "
+        return f"{self.function_name}({concatenate_as_string(self.arguments, delimiter)})"
+
+    @property
+    def value(self):
+        return str(self)
+
+
+def invoke(function_name, *arguments):
+    arguments = [argument.identifier if isinstance(argument, Variable) else argument for argument in arguments]
+    return Expression(FunctionCall(function_name, arguments))
+
+
+@dataclass
+class Include:
+    file_name: str
+
+    def __repr__(self):
+        return f"#include <{self.file_name}>"
+
+
+@dataclass
+class NewLine:
+    amount: int = 1
+
+    def __repr__(self):
+        return "\n" * (self.amount - 1)
+
+
+@dataclass
+class File:
+    name: str
+    objects: list[Union[Include, Variable, Function]]
+
+    def __repr__(self):
+        delimiter = "\n"
+        return f"{concatenate_as_string(self.objects, delimiter)}\n"
+
+
+__all__ = [
+    "Expression",
+    "literal",
+    "BinaryOperation",
+    "Identifier",
+    "Type",
+    "Variable",
+    "variable",
+    "AUTO",
+    "Subscript",
+    "Statement",
+    "Return",
+    "Assign",
+    "AssignUpdate",
+    "add_in_place",
+    "Declare",
+    "Block",
+    "ForLoop",
+    "Function",
+    "FunctionCall",
+    "invoke",
+    "Include",
+    "NewLine",
+    "File",
+]
---
+++ codegen/codegen_test.py
@@ -0,0 +1,290 @@
+import codegen as c
+
+
+def test_declare_variable():
+    index = c.variable(c.AUTO, "index") << c.literal(0)
+    expected = "auto index = 0;"
+    assert str(index) == expected
+
+
+def test_function_that_gets_array_element_at_index():
+
+    array = c.variable(c.Type("float").pointer(), "array")
+    index = c.variable(c.Type("int"), "index")
+    result = c.variable(c.AUTO, "result")
+
+    function = c.Function(
+        return_type=c.Type("float"),
+        name=c.Identifier("get"),
+        arguments=[array, index],
+        body=c.Block(
+            [
+                c.Statement(c.Declare(result, array[index])),
+                c.Return(c.Statement(result.value)),
+            ]
+        ),
+    )
+
+    expected = """\
+float get(float* array, int index)
+{
+    auto result = array[index];
+    return result;
+}"""
+
+    assert str(function) == expected
+
+
+def test_function_that_reduces_array_using_sum():
+
+    array = c.variable(c.Type("float").pointer().const().restrict(), "array")
+    index = c.variable(c.Type("int"), "index")
+    result = c.variable(c.AUTO, "result")
+
+    function = c.Function(
+        return_type=c.Type("float"),
+        name=c.Identifier("get"),
+        arguments=[array, index],
+        body=c.Block(
+            [
+                c.Statement(c.Declare(result, c.literal(0))),
+                c.ForLoop(
+                    c.Declare(index, c.literal(0)),
+                    index < c.literal(10),
+                    c.add_in_place(index, c.literal(1)),
+                    c.Block(
+                        [
+                            c.Statement(c.add_in_place(result, array[index])),
+                        ]
+                    ),
+                ),
+                c.Return(c.Statement(result.value)),
+            ]
+        ),
+    )
+
+    expected = """\
+float get(const float* __restrict__ array, int index)
+{
+    auto result = 0;
+    for (int index = 0; index < 10; index++)
+    {
+        result += array[index];
+    }
+    return result;
+}"""
+
+    assert str(function) == expected
+
+
+def _mm256_reduce_add_ps():
+    Vector256Type = c.Type("__m256")
+    Vector128Type = c.Type("__m128")
+    x = c.variable(Vector256Type, "x")
+    x128 = c.variable(Vector128Type.const(), "x128")
+    x64 = c.variable(Vector128Type.const(), "x64")
+    x32 = c.variable(Vector128Type.const(), "x32")
+
+    return (
+        c.Function(
+            return_type=c.Type("float"),
+            name=c.Identifier("_mm256_reduce_add_ps"),
+            arguments=[x],
+            body=c.Block(
+                [
+                    c.Statement(
+                        c.Declare(
+                            x128,
+                            c.invoke(
+                                c.Identifier("_mm_add_ps"),
+                                c.invoke(c.Identifier("_mm256_extractf128_ps"), x, c.literal(1)),
+                                c.invoke(c.Identifier("_mm256_castps256_ps128"), x),
+                            ),
+                        )
+                    ),
+                    c.Statement(
+                        c.Declare(
+                            x64,
+                            c.invoke(
+                                c.Identifier("_mm_add_ps"),
+                                x128,
+                                c.invoke(c.Identifier("_mm_movehl_ps"), x128, x128),
+                            ),
+                        )
+                    ),
+                    c.Statement(
+                        c.Declare(
+                            x32,
+                            c.invoke(
+                                c.Identifier("_mm_add_ss"),
+                                x64,
+                                c.invoke(c.Identifier("_mm_shuffle_ps"), x64, x64, c.literal("0x55")),
+                            ),
+                        )
+                    ),
+                    c.Return(c.Statement(c.invoke(c.Identifier("_mm_cvtss_f32"), x32))),
+                ]
+            ),
+        )
+        .inline()
+        .static()
+    )
+
+
+def matmul_kernel(avx_size):
+
+    Vector256Type = c.Type("__m256")
+    InputType = c.Type("float").const().pointer().restrict().aligned("ALIGNMENT")
+    OutputType = c.Type("float").pointer().restrict().aligned("ALIGNMENT")
+    input_a = c.variable(InputType, "input_a")
+    input_b = c.variable(InputType, "input_b")
+    output = c.variable(OutputType, "output")
+
+    input_a_vector = c.variable(Vector256Type, "input_a_vector")
+    input_b_vector = c.variable(Vector256Type, "input_b_vector")
+    output_vector = c.variable(Vector256Type, "output_vector")
+
+    b = c.variable(c.AUTO, "b")
+    m = c.variable(c.AUTO, "m")
+    n = c.variable(c.AUTO, "n")
+    k = c.variable(c.AUTO, "k")
+
+    kernel_body = c.Block(
+        [
+            c.Statement(
+                c.Declare(
+                    input_a_vector,
+                    c.invoke(
+                        c.Identifier("_mm256_load_ps"),
+                        input_a + m * c.literal(64) + k,
+                    ),
+                )
+            ),
+            c.Statement(
+                c.Declare(
+                    input_b_vector,
+                    c.invoke(
+                        c.Identifier("_mm256_load_ps"),
+                        input_b + n * c.literal(64) + k,
+                    ),
+                )
+            ),
+            c.Statement(
+                c.Assign(
+                    output_vector,
+                    c.invoke(
+                        c.Identifier("_mm256_fmadd_ps"),
+                        input_a_vector,
+                        input_b_vector,
+                        output_vector,
+                    ),
+                )
+            ),
+        ]
+    )
+    k_for_loop = c.ForLoop(
+        c.Declare(k, c.literal(0)),
+        k < c.literal(64),
+        c.add_in_place(k, avx_size),
+        kernel_body,
+    )
+
+    n_for_loop = c.ForLoop(
+        c.Declare(n, c.literal(0)),
+        n < c.literal(64),
+        c.add_in_place(n, c.literal(1)),
+        c.Block(
+            [
+                c.Statement(
+                    c.Declare(
+                        output_vector,
+                        c.invoke(c.Identifier("_mm256_setzero_ps")),
+                    )
+                ),
+                k_for_loop,
+                c.Statement(
+                    c.add_in_place(
+                        output[m * c.literal(64) + n],
+                        c.invoke(c.Identifier("_mm256_reduce_add_ps"), output_vector),
+                    )
+                ),
+            ]
+        ),
+    )
+
+    m_for_loop = c.ForLoop(
+        c.Declare(m, c.literal(0)),
+        m < c.literal(64),
+        c.add_in_place(m, c.literal(1)),
+        c.Block([n_for_loop]),
+    )
+    b_for_loop = c.ForLoop(
+        c.Declare(b, c.literal(0)),
+        b < c.literal(1),
+        c.add_in_place(b, c.literal(1)),
+        c.Block([m_for_loop]),
+    )
+
+    return c.Function(
+        return_type=c.Type("void"),
+        name=c.Identifier("MatmulKernel"),
+        arguments=[input_a, input_b, output],
+        body=c.Block(
+            [
+                b_for_loop,
+            ]
+        ),
+    )
+
+
+def test_matmul_kernel_file():
+    avx_size = c.variable(c.AUTO.constexpr(), "AVX_SIZE")
+    file = c.File(
+        "matmul.hpp",
+        [
+            c.Include("immintrin.h"),
+            c.NewLine(),
+            avx_size << c.literal(8),
+            c.NewLine(),
+            _mm256_reduce_add_ps(),
+            c.NewLine(),
+            matmul_kernel(avx_size),
+        ],
+    )
+
+    expected = """\
+#include <immintrin.h>
+
+constexpr auto AVX_SIZE = 8;
+
+static inline float _mm256_reduce_add_ps(__m256 x)
+{
+    const __m128 x128 = _mm_add_ps(_mm256_extractf128_ps(x, 1), _mm256_castps256_ps128(x));
+    const __m128 x64 = _mm_add_ps(x128, _mm_movehl_ps(x128, x128));
+    const __m128 x32 = _mm_add_ss(x64, _mm_shuffle_ps(x64, x64, 0x55));
+    return _mm_cvtss_f32(x32);
+}
+
+void MatmulKernel(const float* __restrict__ __attribute__((aligned(ALIGNMENT))) input_a, const float* __restrict__ __attribute__((aligned(ALIGNMENT))) input_b, float* __restrict__ __attribute__((aligned(ALIGNMENT))) output)
+{
+    for (auto b = 0; b < 1; b++)
+    {
+        for (auto m = 0; m < 64; m++)
+        {
+            for (auto n = 0; n < 64; n++)
+            {
+                __m256 output_vector = _mm256_setzero_ps();
+                for (auto k = 0; k < 64; k += AVX_SIZE)
+                {
+                    __m256 input_a_vector = _mm256_load_ps(input_a + m * 64 + k);
+                    __m256 input_b_vector = _mm256_load_ps(input_b + n * 64 + k);
+                    output_vector = _mm256_fmadd_ps(input_a_vector, input_b_vector, output_vector);
+                }
+                output[m * 64 + n] += _mm256_reduce_add_ps(output_vector);
+            }
+        }
+    }
+}
+"""
+
+    assert str(file) == expected
