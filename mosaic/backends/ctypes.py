0aacd7ed3b07723b7a4072c211e9e8a1a577cdaf
arakhmati <akhmed.rakhmati@gmail.com>
2023-04-23 17:21:36 -0400

    mosaic/backends/x86/ctypes: updated cast_numpy_array_to_pointer to be more generic and cast based on the dtype

--- mosaic/backends/ctypes.py
+++ mosaic/backends/ctypes.py
@@ -1,6 +1,13 @@
-from ctypes import POINTER, c_float
+from ctypes import POINTER, c_float, c_int64
+
+import numpy as np
 
 
 def cast_numpy_array_to_pointer(flat_array):
-    c_float_p = POINTER(c_float)
-    return flat_array.ctypes.data_as(c_float_p)
+    numpy_dtype_to_c_type = {
+        np.dtype(np.float32): c_float,
+        np.dtype(np.int64): c_int64,
+    }
+    c_type = numpy_dtype_to_c_type[flat_array.dtype]
+    pointer_type = POINTER(c_type)
+    return flat_array.ctypes.data_as(pointer_type)
