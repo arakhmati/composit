96e1a3b36b654e6297bcadc536c5eadbdcc6428c
arakhmati <akhmed.rakhmati@gmail.com>
2023-05-23 22:16:44 -0400

    composit/nn/functions: commonized pool functions. Added pool_last_channels

--- composit/nn/functions.py
+++ composit/nn/functions.py
@@ -1,5 +1,6 @@
-import numpy as np
+import math
 
+import numpy as np
 
 from composit.multidigraph import MultiDiGraph
 from composit.nn.core import Variable, wrap_as_instruction
@@ -115,56 +116,95 @@ def convolution(image, filters, *, data_format, strides=(1, 1), padding=(0, 0)):
     return function(image, filters, strides, padding)
 
 
-@wrap_as_instruction()
-def average_pooling(image, *, kernel_size):
-    # TODO: Make average_pooling generic for all dimensions?
+def pool_output_dim(input_dim, kernel_dim, stride, pool_function):
+    if pool_function == np.max:
+        return math.floor((input_dim - (kernel_dim - 1) - 1) / stride + 1)
+    else:
+        return math.floor((input_dim - kernel_dim) / stride + 1)
+
+
+def pool_channels_first(image, *, kernel_size, strides, padding, pool_function):
+    padding_height, padding_width = padding
+    image = np.pad(
+        image,
+        ((0, 0), (0, 0), (padding_height, padding_height), (padding_width, padding_width)),
+        mode="constant",
+        constant_values=-np.inf if pool_function == np.max else 0,
+    )
 
     batch_size, num_channels, height, width = image.shape
     kernel_height, kernel_width = kernel_size
 
-    output_height = (height - kernel_size[0]) + 1
-    output_width = (width - kernel_size[1]) + 1
+    strides_height, strides_width = strides
+
+    output_height = pool_output_dim(height, kernel_height, strides_height, pool_function)
+    output_width = pool_output_dim(width, kernel_width, strides_width, pool_function)
 
     output = np.zeros((batch_size, num_channels, output_height, output_width), dtype=image.dtype)
     for batch_index in range(batch_size):
         for channel_index in range(num_channels):
             for output_height_index in range(output_height):
                 for output_width_index in range(output_width):
-                    output[batch_index, channel_index, output_height_index, output_width_index] = np.mean(
+                    output[batch_index, channel_index, output_height_index, output_width_index] = pool_function(
                         image[
                             batch_index,
                             channel_index,
-                            output_height_index : output_height_index + kernel_height,
-                            output_width_index : output_width_index + kernel_width,
+                            output_height_index * strides_height : output_height_index * strides_height + kernel_height,
+                            output_width_index * strides_width : output_width_index * strides_width + kernel_width,
+                        ]
+                    )
+    return output
+
+
+def pool_channels_last(image, *, kernel_size, strides, padding, pool_function):
+    padding_height, padding_width = padding
+    image = np.pad(
+        image,
+        ((0, 0), (padding_height, padding_height), (padding_width, padding_width), (0, 0)),
+        mode="constant",
+        constant_values=-np.inf if pool_function == np.max else 0,
+    )
+
+    batch_size, height, width, num_channels = image.shape
+    kernel_height, kernel_width = kernel_size
+
+    strides_height, strides_width = strides
+
+    output_height = pool_output_dim(height, kernel_height, strides_height, pool_function)
+    output_width = pool_output_dim(width, kernel_width, strides_width, pool_function)
+
+    output = np.zeros((batch_size, output_height, output_width, num_channels), dtype=image.dtype)
+    for batch_index in range(batch_size):
+        for output_height_index in range(output_height):
+            for output_width_index in range(output_width):
+                for channel_index in range(num_channels):
+                    output[batch_index, output_height_index, output_width_index, channel_index] = pool_function(
+                        image[
+                            batch_index,
+                            output_height_index * strides_height : output_height_index * strides_height + kernel_height,
+                            output_width_index * strides_width : output_width_index * strides_width + kernel_width,
+                            channel_index,
                         ]
                     )
     return output
 
 
 @wrap_as_instruction()
-def max_pooling(image, *, kernel_size):
-    # TODO: Make max_pooling generic for all dimensions?
+def pool(image, *, pool_function, kernel_size, data_format, strides=(1, 1), padding=(0, 0)):
+    data_format_to_function = {
+        "NCHW": pool_channels_first,
+        "NHWC": pool_channels_last,
+    }
+    function = data_format_to_function[data_format]
+    return function(image, kernel_size=kernel_size, strides=strides, padding=padding, pool_function=pool_function)
 
-    batch_size, num_channels, height, width = image.shape
-    kernel_height, kernel_width = kernel_size
 
-    output_height = (height - kernel_size[0]) + 1
-    output_width = (width - kernel_size[1]) + 1
+def average_pool(*args, **kwargs):
+    return pool(*args, **kwargs, pool_function=np.mean)
 
-    output = np.zeros((batch_size, num_channels, output_height, output_width), dtype=image.dtype)
-    for batch_index in range(batch_size):
-        for channel_index in range(num_channels):
-            for output_height_index in range(output_height):
-                for output_width_index in range(output_width):
-                    output[batch_index, channel_index, output_height_index, output_width_index] = np.max(
-                        image[
-                            batch_index,
-                            channel_index,
-                            output_height_index : output_height_index + kernel_height,
-                            output_width_index : output_width_index + kernel_width,
-                        ]
-                    )
-    return output
+
+def max_pool(*args, **kwargs):
+    return pool(*args, **kwargs, pool_function=np.max)
 
 
 __all__ = [
@@ -173,6 +213,6 @@ __all__ = [
     "gelu",
     "relu",
     "convolution",
-    "average_pooling",
-    "max_pooling",
+    "average_pool",
+    "max_pool",
 ]
