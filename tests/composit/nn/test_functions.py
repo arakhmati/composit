39c162cc9f1b4aba9f62865782028b4277e9641e
arakhmati <akhmed.rakhmati@gmail.com>
2023-05-23 22:16:44 -0400

    composit/nn/functions: commonized pool functions. Added pool_last_channels

--- tests/composit/nn/test_functions.py
+++ tests/composit/nn/test_functions.py
@@ -71,23 +71,36 @@ def test_convolution(strides, padding, data_format):
     assert np.allclose(result, torch_result)
 
 
-def test_average_pooling():
-    image = cnp.random.random((1, 3, 28, 28))
-    result = cnp.nn.average_pooling(image, kernel_size=(2, 2))
+@pytest.mark.parametrize(
+    "composit_and_torch_functions",
+    [(cnp.nn.average_pool, torch.nn.functional.avg_pool2d), (cnp.nn.max_pool, torch.nn.functional.max_pool2d)],
+)
+@pytest.mark.parametrize("kernel_size", [(2, 2), (3, 3)])
+@pytest.mark.parametrize("strides", [(1, 1), (2, 2)])
+@pytest.mark.parametrize("padding", [(0, 0), (1, 1)])
+@pytest.mark.parametrize("data_format", ["NCHW", "NHWC"])
+def test_pool(composit_and_torch_functions, kernel_size, strides, padding, data_format):
+    composit_function, torch_function = composit_and_torch_functions
 
-    torch_image = torch.from_numpy(cnp.evaluate(image))
-    torch_result = torch.nn.functional.avg_pool2d(torch_image, kernel_size=[2, 2], stride=[1, 1]).numpy()
+    np_image = np.random.random((1, 3, 28, 38))
+
+    image_var = image_arg = cnp.nn.variable(name="image", shape=np_image.shape, dtype=np_image.dtype)
+
+    if data_format == "NHWC":
+        image_arg = cnp.transpose(image_var, (0, 2, 3, 1))
+
+    result_var = composit_function(
+        image_arg, kernel_size=kernel_size, strides=strides, padding=padding, data_format=data_format
+    )
+
+    if data_format == "NHWC":
+        result_var = cnp.transpose(result_var, (0, 3, 1, 2))
+
+    result = cnp.nn.evaluate(result_var, inputs={image_var: np_image})
+
+    torch_result = torch_function(
+        torch.from_numpy(np_image), kernel_size=kernel_size, stride=strides, padding=padding
+    ).numpy()
 
     assert result.shape == torch_result.shape
-    assert np.allclose(cnp.evaluate(result), torch_result)
-
-
-def test_max_pooling():
-    image = cnp.random.random((1, 3, 28, 28))
-    result = cnp.nn.max_pooling(image, kernel_size=(2, 2))
-
-    torch_image = torch.from_numpy(cnp.evaluate(image))
-    torch_result = torch.nn.functional.max_pool2d(torch_image, kernel_size=[2, 2], stride=[1, 1]).numpy()
-
-    assert result.shape == torch_result.shape
-    assert np.allclose(cnp.evaluate(result), torch_result)
+    assert np.allclose(result, torch_result)
