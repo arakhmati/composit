9ad6eb9a572d4f9541ddc9309fa78ebc8c2cff97
arakhmati <akhmed.rakhmati@gmail.com>
2023-06-18 22:44:14 -0400

    flashlight/tracer: added ability to trace without computing torch tensors

--- tests/flashlight/test_tracer.py
+++ tests/flashlight/test_tracer.py
@@ -1,73 +1,97 @@
+import pytest
+
 import torch
 
+import composit as cnp
 import flashlight
 
 
-def test_add():
+def check_output_type(output, run_torch):
+    if run_torch:
+        assert isinstance(output, flashlight.Tensor)
+    else:
+        assert isinstance(output, cnp.types.LazyTensor)
+
+
+@pytest.mark.parametrize("run_torch", [True, False])
+def test_add(run_torch):
     input_a = torch.rand(32, 128)
     input_b = torch.rand(128)
 
-    with flashlight.tracer.trace():
+    with flashlight.tracer.trace(run_torch):
         output = input_a + input_b
 
+    check_output_type(output, run_torch)
     assert len(output.graph) == 3
 
 
-def test_sub():
+@pytest.mark.parametrize("run_torch", [True, False])
+def test_sub(run_torch):
     input_a = torch.rand(32, 128)
     input_b = torch.rand(128)
 
-    with flashlight.tracer.trace():
+    with flashlight.tracer.trace(run_torch):
         output = input_a - input_b
 
+    check_output_type(output, run_torch)
     assert len(output.graph) == 3
 
 
-def test_mul():
+@pytest.mark.parametrize("run_torch", [True, False])
+def test_mul(run_torch):
     input_a = torch.rand(32, 128)
     input_b = torch.rand(128)
 
-    with flashlight.tracer.trace():
+    with flashlight.tracer.trace(run_torch):
         output = input_a * input_b
 
+    check_output_type(output, run_torch)
     assert len(output.graph) == 3
 
 
-def test_truediv():
+@pytest.mark.parametrize("run_torch", [True, False])
+def test_truediv(run_torch):
     input_a = torch.rand(32, 128)
     input_b = torch.rand(128)
 
-    with flashlight.tracer.trace():
+    with flashlight.tracer.trace(run_torch):
         output = input_a / input_b
 
+    check_output_type(output, run_torch)
     assert len(output.graph) == 3
 
 
-def test_matmul():
+@pytest.mark.parametrize("run_torch", [True, False])
+def test_matmul(run_torch):
     input_a = torch.rand(32, 128)
     input_b = torch.rand(128, 64)
 
-    with flashlight.tracer.trace():
+    with flashlight.tracer.trace(run_torch):
         output = input_a @ input_b
 
+    check_output_type(output, run_torch)
     assert len(output.graph) == 3
 
 
-def test_linear():
+@pytest.mark.parametrize("run_torch", [True, False])
+def test_linear(run_torch):
     model = torch.nn.Linear(128, 64)
     activations = torch.rand(32, 128)
 
-    with flashlight.tracer.trace():
+    with flashlight.tracer.trace(run_torch):
         output = model(activations)
 
+    check_output_type(output, run_torch)
     assert len(output.graph) == 6
 
 
-def test_embedding():
+@pytest.mark.parametrize("run_torch", [True, False])
+def test_embedding(run_torch):
     model = torch.nn.Embedding(num_embeddings=10, embedding_dim=64)
     input_ids = torch.randint(0, 10, (1, 128))
 
-    with flashlight.tracer.trace():
+    with flashlight.tracer.trace(run_torch):
         output = model(input_ids)
 
+    check_output_type(output, run_torch)
     assert len(output.graph) == 3
