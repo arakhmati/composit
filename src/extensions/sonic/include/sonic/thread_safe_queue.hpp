0dc28dbda22973910b4c3b3166ddb8cc0e16ad79
arakhmati <akhmed.rakhmati@gmail.com>
2023-08-21 01:13:38 -0400

    sonic: added thread_safe_queue

---
+++ src/extensions/sonic/include/sonic/thread_safe_queue.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <mutex>
+#include <optional>
+#include <queue>
+
+namespace sonic {
+
+namespace thread_safe_queue {
+
+template <class T>
+struct thread_safe_queue_t {
+  ~thread_safe_queue_t() {}
+
+  void push_back(T value) {
+    std::scoped_lock<std::mutex> lock(mutex);
+    this->queue.push(value);
+  }
+
+  std::optional<T> pop_front() {
+    std::unique_lock<std::mutex> lock(mutex);
+    if (this->queue.empty()) {
+      return std::nullopt;
+    }
+    auto value = this->queue.front();
+    this->queue.pop();
+    return value;
+  }
+
+  bool empty() const {
+    std::scoped_lock<std::mutex> lock(mutex);
+    return this->queue.empty();
+  }
+
+  std::size_t size() const {
+    std::scoped_lock<std::mutex> lock(mutex);
+    return this->queue.size();
+  }
+
+  std::queue<T> queue;
+  mutable std::mutex mutex;
+};
+
+}  // namespace thread_safe_queue
+
+}  // namespace sonic
