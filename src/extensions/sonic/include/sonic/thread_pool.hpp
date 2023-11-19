b7eaa1bb293582d40f0ae19948e26b6e9e4e25ba
arakhmati <akhmed.rakhmati@gmail.com>
2023-11-19 15:23:05 -0500

    sonic: compute matmul using different threads

--- src/extensions/sonic/include/sonic/thread_pool.hpp
+++ src/extensions/sonic/include/sonic/thread_pool.hpp
@@ -4,6 +4,7 @@
 
 #include <array>
 #include <atomic>
+#include <functional>
 #include <thread>
 
 namespace sonic {
@@ -14,20 +15,24 @@ template <auto num_threads>
 class thread_pool_t {
   std::array<std::atomic<bool>, num_threads> index_to_run_thread;
   std::array<std::thread, num_threads> index_to_thread;
-  sonic::thread_safe_queue::thread_safe_queue_t<std::function<void(std::size_t)>> work_queue;
+  sonic::thread_safe_queue::thread_safe_queue_t<std::function<void(std::size_t)>> computation_queue;
+  std::atomic<std::size_t> num_remaining_computations;
 
  public:
   thread_pool_t() {
-    auto& work_queue = this->work_queue;
     std::fill(std::begin(this->index_to_run_thread), std::end(this->index_to_run_thread), true);
+    this->num_remaining_computations = 0;
+    auto& computation_queue = this->computation_queue;
+    auto& num_remaining_computations = this->num_remaining_computations;
     for (auto index = 0; index < num_threads; index++) {
       auto& run_thread = this->index_to_run_thread[index];
-      this->index_to_thread[index] = std::thread([&work_queue, &run_thread, index]() {
+      this->index_to_thread[index] = std::thread([&computation_queue, &run_thread, &num_remaining_computations, index] {
         while (run_thread) {
-          auto optional_computation = work_queue.pop_front();
+          auto optional_computation = computation_queue.pop_front();
           if (optional_computation.has_value()) {
             auto computation = optional_computation.value();
             computation(index);
+            num_remaining_computations--;
           } else {
             std::this_thread::yield();
           }
@@ -37,8 +42,7 @@ class thread_pool_t {
   }
 
   ~thread_pool_t() {
-    while (not this->work_queue.empty()) {
-    }
+    this->synchronize();
 
     std::fill(std::begin(this->index_to_run_thread), std::end(this->index_to_run_thread), false);
     for (auto& thread : this->index_to_thread) {
@@ -46,7 +50,15 @@ class thread_pool_t {
     }
   }
 
-  void push_back_computation(auto&& computation) { this->work_queue.push_back(computation); }
+  void push_back_computation(auto&& computation) {
+    num_remaining_computations++;
+    this->computation_queue.push_back(computation);
+  }
+
+  void synchronize() {
+    while (this->num_remaining_computations > 0) {
+    }
+  }
 };
 
 }  // namespace thread_pool
