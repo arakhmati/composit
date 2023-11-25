e719663489fbc6dddc24a4987ee1d7ce873bc184
arakhmati <akhmed.rakhmati@gmail.com>
2023-11-24 22:15:03 -0500

    sonic: speed up align_array by returning early when the array is already aligned.

--- src/mosaic/aligned_array.py
+++ src/mosaic/aligned_array.py
@@ -9,7 +9,17 @@ def create_aligned_array(shape, dtype, alignment=32):
     return array
 
 
-def align_array(array, alignment=32):
-    aligned_array = create_aligned_array(array.shape, array.dtype, alignment=alignment)
+def create_aligned_array_like(array, alignment=32):
+    size = array.itemsize * array.size
+    buffer = np.empty(size + alignment, dtype=np.uint8)
+    offset = buffer.ctypes.data % alignment
+    array = np.ndarray(array.shape, dtype=array.dtype, buffer=buffer, offset=offset)
+    return array
+
+
+def align_array(array, alignment=1024):
+    if array.__array_interface__["data"][0] % alignment == 0:
+        return array
+    aligned_array = create_aligned_array_like(array, alignment=alignment)
     aligned_array[:] = array
     return aligned_array
